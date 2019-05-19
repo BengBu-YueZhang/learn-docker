764b9a8bd6d9d3cceea0b459e66b33476d08d221
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-19 18:02:41 +0800

    init

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const app = new Koa()
+const router = new Router()
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const config = require('./config')
+const routes = require('./routes')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser())
+  .use(json())
+  .use(logger())
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - $ms`)
+})
+
+router.get('/', async (ctx, next) => {
+  // ctx.body = 'Hello World'
+  ctx.state = {
+    title: 'Koa2'
+  }
+  await ctx.render('index', ctx.state)
+})
+
+routes(router)
+app.on('error', function(err, ctx) {
+  console.log(err)
+  logger.error('server error', err, ctx)
+})
+
+module.exports = app.listen(config.port, () => {
+  console.log(`Listening on http://localhost:${config.port}`)
+})
